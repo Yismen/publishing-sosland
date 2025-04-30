d5213a4e7e64488f1fb74b43f3d78a61a3b50ff5
Yismen Jorge <yismen.jorge@gmail.com>
2025-04-29 20:26:44 -0400

    Add Filament Log Manager and Env Editor plugins; refactor AdminPanelProvider to integrate new features and update CSS styles for improved UI.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,23 +2,25 @@
 
 namespace App\Providers\Filament;
 
-use App\Services\BreezeCoreService;
-use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\AuthenticateSession;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
+use Filament\Widgets;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
-use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Routing\Middleware\SubstituteBindings;
+use App\Services\BreezeCoreService;
+use Filament\Http\Middleware\Authenticate;
 use Illuminate\Session\Middleware\StartSession;
-use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Illuminate\Cookie\Middleware\EncryptCookies;
 use Stephenjude\FilamentDebugger\DebuggerPlugin;
+use Filament\Http\Middleware\AuthenticateSession;
+use FilipFonal\FilamentLogManager\FilamentLogManager;
+use GeoSot\FilamentEnvEditor\FilamentEnvEditorPlugin;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -59,6 +61,33 @@ class AdminPanelProvider extends PanelProvider
                     ->telescopeNavigation(true)
                     ->horizonNavigation(false)
                     ->pulseNavigation(false),
+                FilamentLogManager::make(),
+                FilamentEnvEditorPlugin::make()
+                    ->navigationGroup('Settings')
+                    ->navigationLabel('Env Editor')
+                    ->navigationIcon('heroicon-o-document-text')
+                    ->navigationSort(1)
+                    ->hideKeys(
+                        'APP_KEY',
+                        'APP_URL',
+                        'DB_PASSWORD',
+                        'DB_DATABASE',
+                        'DB_HOST',
+                        'DB_USERNAME',
+                        'DB_CONNECTION',
+                        'DB_HOST',
+                        'DB_PORT',
+                        'MAIL_PASSWORD',
+                        'MAIL_USERNAME',
+                        'MAIL_HOST',
+                        'MAIL_PORT',
+                        'MAIL_ENCRYPTION',
+                        'MAIL_FROM_ADDRESS',
+                        'MAIL_FROM_NAME',
+                        'MAIL_MAILER',
+                        'MAIL_ENCRYPTION',
+
+                    ),
             ])
             ->authMiddleware([
                 Authenticate::class,
